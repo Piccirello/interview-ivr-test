d897a1bb44b5b5d17e8ca5dce76d11377a436a5f
Thomas Piccirello <thomas@piccirello.com>
2019-04-29 23:46:29 -0700

    Remove unused comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-#!/usr/bin/env node
-
 'use strict';
 
-//import dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -10,18 +7,14 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 
-// define the Express app
 const app = express();
 
-// enhance app security with Helmet
 app.use(helmet());
 app.use(helmet.noCache());
 
-// use bodyParser to parse application/json content-type
 app.use(bodyParser.json({limit: '1mb'}));
 app.use(bodyParser.urlencoded({extended: true }));
 
-// TODO should we make this more strict? only allow app.aiinsurance.io? config: https://github.com/expressjs/cors
 // enable all CORS requests
 app.use(cors());
 
